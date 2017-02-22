f0ad2554a90e3e52ccdef9aeacea6fde6244465e
barnesgia <barnesgia@gmail.com>
2017-02-22 16:55:01 -0500

    upload1

---
+++ DevUSimpleCalculator/Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DevUSimpleCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-weight: normal;
+        }
+        .auto-style2 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h1 class="auto-style1"><strong>Simple Calculator</strong></h1>
+    
+    </div>
+        <p>
+            <span class="auto-style2">First Value:</span>&nbsp;
+            <asp:TextBox ID="firstValue" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <span class="auto-style2">Second Value:</span>&nbsp;
+            <asp:TextBox ID="secondValue" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="add" runat="server" OnClick="add_Click" Text="+" />
+&nbsp;&nbsp;
+            <asp:Button ID="subtract" runat="server" OnClick="subtract_Click" Text="-" />
+&nbsp;&nbsp;
+            <asp:Button ID="mult" runat="server" OnClick="mult_Click" Text="*" />
+&nbsp;&nbsp;
+            <asp:Button ID="divide" runat="server" OnClick="divide_Click" style="height: 29px" Text="/" />
+        </p>
+        <asp:Label ID="result" runat="server" style="background-color: #66CCFF"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ DevUSimpleCalculator/Default.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DevUSimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void add_Click(object sender, EventArgs e)
+        {
+            string firstStr = firstValue.Text;
+            string secondStr = secondValue.Text;
+            int firstNum = int.Parse(firstStr);
+            int secondNum = int.Parse(secondStr);
+
+            int resultNumber;
+
+            checked
+            {
+                resultNumber = firstNum + secondNum;
+            }
+
+            result.Text = resultNumber.ToString();
+        }
+
+        protected void subtract_Click(object sender, EventArgs e)
+        {
+            string firstStr = firstValue.Text;
+            string secondStr = secondValue.Text;
+            int firstNum = int.Parse(firstStr);
+            int secondNum = int.Parse(secondStr);
+
+            int resultNumber;
+
+            checked
+            {
+                resultNumber = firstNum - secondNum;
+            }
+
+            result.Text = resultNumber.ToString();
+        }
+
+        protected void mult_Click(object sender, EventArgs e)
+        {
+            string firstStr = firstValue.Text;
+            string secondStr = secondValue.Text;
+            int firstNum = int.Parse(firstStr);
+            int secondNum = int.Parse(secondStr);
+
+            int resultNumber;
+
+            checked
+            {
+                resultNumber = firstNum * secondNum;
+            }
+
+            result.Text = resultNumber.ToString();
+        }
+
+        protected void divide_Click(object sender, EventArgs e)
+        {
+            string firstStr = firstValue.Text;
+            string secondStr = secondValue.Text;
+            double firstNum = double.Parse(firstStr);
+            double secondNum = double.Parse(secondStr);
+
+            double resultNumber;
+
+            checked
+            {
+                resultNumber = firstNum / secondNum;
+            }
+
+            result.Text = resultNumber.ToString();
+        }
+    }
+}
